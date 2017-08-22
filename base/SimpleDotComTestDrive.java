0e753387daabbfacb184909be99959b957aa5652
guowei2 <guowei2@pset.suntec.net>
2017-08-22 15:48:43 +0800

    [base] modify SimpleDotComTestDrive.java and add real interactive design

--- base/SimpleDotComTestDrive.java
+++ base/SimpleDotComTestDrive.java
@@ -1,16 +1,55 @@
+import java.io.*;
 
 public class SimpleDotComTestDrive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SimpleDotCom dot = new SimpleDotCom();
-		int[] locations = {2, 3, 4};
-		dot.setLocationCells(locations);
-		String userGuess = "5";
-		String result = dot.checkYourself(userGuess);
+		//SimpleDotCom dot = new SimpleDotCom();
+		//int[] locations = {2, 3, 4};
+		//dot.setLocationCells(locations);
+		//String userGuess = "5";
+		//String result = dot.checkYourself(userGuess);
+		
+		int numOfGuesses = 0;
+		GameHelper helper = new GameHelper();
+		SimpleDotCom theDotCom = new SimpleDotCom();
+		int randomNum = (int)(Math.random() * 5);
+		int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+		theDotCom.setLocationCells(locations);
+		boolean isAlive = true;
+		
+		while(isAlive == true)
+		{
+			String guess = helper.getUserInput("enter a numper");
+			String result = theDotCom.checkYourself(guess);
+			numOfGuesses++;
+			if(result.equals("kill"))
+			{
+				isAlive = false;
+				System.out.println("You took " + numOfGuesses + " guesses");
+			}
+		}
 
 	}
 
+}
+
+class GameHelper
+{
+	String getUserInput(String prompt)
+	{
+		String inputLine = null;
+		System.out.print(prompt + "  ");
+		try {
+			BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+			inputLine = is.readLine();
+			if(inputLine.length() == 0)
+				return null;
+		}catch(IOException e) {
+			System.out.println("IOException: " + e);
+		}
+		return inputLine;
+	}
 }
 
 class SimpleDotCom
