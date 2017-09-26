3468b4a4935121d04dc2997e0f6c7bfa5f0b3147
guowei2 <guowei2@pset.suntec.net>
2017-09-26 18:10:25 +0800

    [design] add factory method pattern

--- design/FactoryPattern.java
+++ design/FactoryPattern.java
@@ -3,6 +3,7 @@
  *分为简单工厂模式、工厂方法模式、抽象工厂模式。
  */
 
+/*
 public class FactoryPattern
 {
     public static void main(String[] args)
@@ -70,6 +71,105 @@ class CheesePizza extends Pizza
 class ClamPizza extends Pizza
 {
     void declare(){sop("This is ClamPizza");}
+}
+*/
+
+
+
+//Factory Method Pattern
+public class FactoryPattern
+{
+    public static void main(String[] args)
+    {
+        NyPizzaStore nps = new NyPizzaStore();
+        nps.orderPizza("NyStyleCheesePizza");
+        nps.orderPizza("NyStyleClamPizza");
+
+        ChicagoPizzaStore cps = new ChicagoPizzaStore();
+        cps.orderPizza("ChicagoStyleCheesePizza");
+        cps.orderPizza("ChicagoStyleClamPizza");
+    }
+}
+
+
+abstract class PizzaStore
+{
+    void orderPizza(String type)
+    {
+        Pizza pizza = createPizza(type);
+
+        pizza.declare();
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+    }
+
+    abstract Pizza createPizza(String type);
+}
+
+class NyPizzaStore extends PizzaStore
+{
+    Pizza createPizza(String type)
+    {
+        if(type.equals("NyStyleCheesePizza"))
+            return new NyStyleCheesePizza();
+        else if(type.equals("NyStyleClamPizza"))
+            return new NyStyleClamPizza();
+        else
+            return null;
+    }
+}
+
+class ChicagoPizzaStore extends PizzaStore
+{
+    Pizza createPizza(String type)
+    {
+        if(type.equals("ChicagoStyleCheesePizza"))
+            return new ChicagoStyleCheesePizza();
+        else if(type.equals("ChicagoStyleClamPizza"))
+            return new ChicagoStyleClamPizza();
+        else
+            return null;
+    }
+}
+
+abstract class Pizza
+{
+    abstract void declare();
+    void prepare(){sop("prepare");}
+    void bake(){sop("bake");}
+    void cut(){sop("cut");}
+    void box(){sop("box");}
+
+    void sop(Object obj)
+    {
+        System.out.println(obj);
+    }
+}
+
+class NyStyleCheesePizza extends Pizza
+{
+    void declare(){sop("This is NyStyleCheesePizza");}
+    void cut(){sop("cut NyStyleCheesePizza");}
+}
+
+class NyStyleClamPizza extends Pizza
+{
+    void declare(){sop("This is NyStyleClamPizza");}
+    void bake(){sop("bake NyStyleClamPizza");}
+}
+
+class ChicagoStyleCheesePizza extends Pizza
+{
+    void declare(){sop("This is ChicagoStyleCheesePizza");}
+    void box(){sop("box ChicagoStyleCheesePizza");}
+}
+
+class ChicagoStyleClamPizza extends Pizza
+{
+    void declare(){sop("This is ChicagoStyleClamPizza");}
+    void prepare(){sop("prepare ChicagoStyleClamPizza");}
 }
 
 
