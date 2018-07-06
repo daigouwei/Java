871caadf50b8d001d52b612e18159f69b7d784cd
daigouwei <daigouwei@gmail.com>
2018-07-06 18:16:03 +0800

    leetcode init

---
+++ src/com/gg/leetcode/TwoNum.java
@@ -0,0 +1,10 @@
+package com.gg.leetcode;
+
+/**
+ * Created by guowei on 2018/7/6.
+ */
+public class TwoNum {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
