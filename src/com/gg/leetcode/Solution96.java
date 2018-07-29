cfd2f12453457a2e5d03802911865d93fbf0815c
daigouwei <daigouwei@gmail.com>
2018-07-29 15:04:33 +0800

    不同的二叉搜索树

--- src/com/gg/leetcode/Solution96.java
+++ src/com/gg/leetcode/Solution96.java
@@ -4,13 +4,19 @@ package com.gg.leetcode;
  * Created by guowei on 2018/7/29.
  */
 public class Solution96 {
+    public static void main(String[] args) {
+        System.out.println(new Solution96().numTrees(3));
+    }
+
     public int numTrees(int n) {
-        if (0 == n)
+        if (0 == n || 1 == n)
             return 1;
-        int res = 0;
-        for (int i = 0; i <= n - 1; i++) {
-            res += numTrees(n - 1 - i) * numTrees(i);
-        }
-        return res;
+        int[] num = new int[n + 1];
+        num[0] = 1;
+        num[1] = 1;
+        for (int i = 2; i <= n; i++)
+            for (int j = 0; j <= i - 1; j++)
+                num[i] += num[i - 1 - j] * num[j];
+        return num[n];
     }
 }
