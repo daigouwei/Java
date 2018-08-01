a4ab3b7b39c77277c45d87c77d7cf850b8f96dfa
daigouwei <daigouwei@gmail.com>
2018-08-01 10:20:44 +0800

    二叉搜索树两节点最近公共祖先

--- src/com/gg/leetcode/Solution235.java
+++ src/com/gg/leetcode/Solution235.java
@@ -10,7 +10,7 @@ public class Solution235 {
         while (null != root) {
             if (p.val < root.val && q.val < root.val)
                 root = root.left;
-            if (p.val > root.val && q.val > root.val)
+            else if (p.val > root.val && q.val > root.val)
                 root = root.right;
             else
                 return root;
